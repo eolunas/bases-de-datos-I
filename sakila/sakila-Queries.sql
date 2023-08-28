f39b20736d7cf19a755032d4e469be3755e0bf41
Eduardo Luna <eolunas@gmail.com>
2023-08-28 18:53:51 -0500

    [ADD] Sakila-DB and exercices

---
+++ sakila/sakila-Queries.sql
@@ -0,0 +1,114 @@
+-- ----------------------------------------------------------------------------------------
+-- Reportes parte 1: 
+-- ----------------------------------------------------------------------------------------
+-- Obtener el nombre y apellido de los primeros 5 actores disponibles. Utilizar 
+-- alias para mostrar los nombres de las columnas en español. 
+SELECT 
+    first_name Nombre, last_name Apellido
+FROM
+    `sakila`.`actor`
+LIMIT 5;
+
+-- Obtener un listado que incluya nombre, apellido y correo electrónico de los 
+-- clientes (customers) inactivos. Utilizar alias para mostrar los nombres de las 
+-- columnas en español. 
+SELECT 
+    first_name Nombre,
+    last_name Apellido,
+    email 'Correo electrónico'
+FROM
+    sakila.customer
+WHERE
+    customer.active = 1;
+
+-- Obtener un listado de films incluyendo título, año y descripción de los films 
+-- que tienen un rental_duration mayor a cinco. Ordenar por rental_duration de 
+-- mayor a menor. Utilizar alias para mostrar los nombres de las columnas en  español.
+SELECT 
+    title "Título",
+    release_year 'Año de lanzamiento',
+    description "Descripción",
+    rental_duration "Duración de renta"
+FROM
+    sakila.film
+WHERE
+    rental_duration > 5
+ORDER BY rental_duration DESC;
+
+-- Obtener un listado de alquileres (rentals) que se hicieron durante el mes de 
+-- mayo de 2005, incluir en el resultado todas las columnas disponibles.
+SELECT 
+    *
+FROM
+    sakila.rental
+WHERE
+    rental_date BETWEEN '2005-05-01' AND '2005-06-01';
+
+-- ----------------------------------------------------------------------------------------
+-- Reportes parte 2: 
+-- ----------------------------------------------------------------------------------------
+-- Obtener la cantidad TOTAL de alquileres (rentals). Utilizar un alias para mostrarlo
+-- en una columna llamada “cantidad”. 
+SELECT 
+    COUNT(*) "Cantidad"
+FROM
+    sakila.rental;
+    
+-- Obtener la suma TOTAL de todos los pagos (payments). Utilizar un alias para mostrarlo
+-- en una columna llamada “total”, junto a una columna con la cantidad de alquileres con
+-- el alias “Cantidad” y una columna que indique el “Importe promedio” por alquiler. 
+SELECT 
+	COUNT(*) Cantidad,
+    SUM(amount) Total,
+    AVG(amount) "Importe promedio"
+FROM
+    sakila.payment;
+
+-- Generar un reporte que responda la pregunta: ¿cuáles son los diez clientes que más 
+-- dinero gastan y en cuántos alquileres lo hacen? 
+SELECT 
+    CONCAT(customer.first_name, " ", customer.last_name) Nombre,
+    COUNT(*) Cantidad, 
+    SUM(amount) Total
+FROM
+    sakila.payment
+        INNER JOIN
+    sakila.customer ON payment.customer_id = customer.customer_id
+GROUP BY Nombre
+ORDER BY Total DESC
+LIMIT 10;
+
+-- Generar un reporte que indique: ID de cliente, cantidad de alquileres y monto total
+-- para todos los clientes que hayan gastado más de 150 dólares en alquileres. 
+SELECT 
+    customer_id, COUNT(*) Cantidad, SUM(amount) Total
+FROM
+    sakila.payment
+GROUP BY customer_id
+HAVING total > 150;
+
+-- Generar un reporte que muestre por mes de alquiler (rental_date de tabla rental),
+-- la cantidad de alquileres y la suma total pagada (amount de tabla payment) para el 
+-- año de alquiler 2005 (rental_date de tabla rental). 
+SELECT 
+	EXTRACT(month FROM rental_date) Mes,
+    COUNT(*) Cantidad, 
+    SUM(amount) Total
+FROM `sakila`.`rental`
+INNER JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY Mes
+ORDER BY Mes;
+
+-- Generar un reporte que responda a la pregunta: ¿cuáles son los 5 inventarios más 
+-- alquilados? (columna inventory_id en la tabla rental). Para cada una de ellas 
+-- indicar la cantidad de alquileres. 
+SELECT 
+    rental.inventory_id, COUNT(*) Cantidad
+FROM
+    `sakila`.`rental`
+        INNER JOIN
+    inventory ON rental.inventory_id = inventory.inventory_id
+GROUP BY rental.inventory_id
+ORDER BY Cantidad DESC
+LIMIT 5;
+
